5562dc9d50d7c30b87af82606ecfad930a47e12f
Rodrigo Azevedo <rodrigodeazevedosilva@yahoo.com.br>
2025-05-22 23:21:51 -0300

    Mudanças para deixar a aplicação mais leve

--- config/puma.rb
+++ config/puma.rb
@@ -1,23 +1,17 @@
-# Puma pode servir cada requisição em um thread de um conjunto de threads.
-# Os valores padrão são mostrados abaixo.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 2 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Especifica a porta em que o servidor Puma vai escutar
 port ENV.fetch("PORT") { 8080 }
 
-# Especifica o ambiente do servidor
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
-# Especifica o número de processos para usar
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 
-# Use o mesmo diretório para persistir estado do servidor
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Certifique-se de escutar em todos os endereços IP disponíveis
 bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 8080 }}"
 
-# Permite que o Puma seja reiniciado por `rails restart` command
+preload_app!
+
 plugin :tmp_restart
