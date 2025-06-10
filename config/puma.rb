f3fa8804a027bf4655494aa86b1577404df8d6bd
Rodrigo Azevedo <rodrigodeazevedosilva@yahoo.com.br>
2025-06-09 23:57:10 -0300

    Novo ajuste no fly.toml

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 2 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-port ENV.fetch("PORT") { 8080 }
+port ENV.fetch("PORT") { 3000 }
 
 environment ENV.fetch("RAILS_ENV") { "production" }
 
@@ -10,7 +10,7 @@ workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 8080 }}"
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3000 }}"
 
 preload_app!
 
