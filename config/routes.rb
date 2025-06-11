427c9075079b35c64c2f46f70beae7bf6512674d
Rodrigo Azevedo <rodrigodeazevedosilva@yahoo.com.br>
2025-06-10 21:06:09 -0300

    Nova alteração no fly.toml

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-#  devise_for :users
-
   devise_for :users, controllers: { registrations: 'registrations' }
-
-  # post "users", to: "users#create"
-
-   #get "users/:id", to: "users#show", as: :user
-  # get "users/:id/edit", to: "users#edit", as: :edit_user
-  # patch "users/:id", to: "users#update"
 
   get "matchs", to: "matchs#index"
   get "match", to: "matchs#show", as: :match
@@ -15,21 +7,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "dashboard", to: "pages#dashboard", as: :dashboard
 
-  # get "adoptionforms/:id", to: "adoptionforms#show", as: :adoptionform
-  # get "adoptionforms/new", to: "adoptionforms#new", as: :new_adoptionform
-  # post "adoptionforms", to: "adoptionforms#create"
-  # get "adoptionforms/:id/edit", to: "adoptionforms#edit"
-  # patch "adoptionforms/:id", to: "adoptionforms#update"
-
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 
   get 'my_applications', to: 'adoptions#my_applications'
 
@@ -50,7 +28,6 @@ Rails.application.routes.draw do
     resources :chatrooms, only: [:create]
   end
 
-  # get 'favorites', to: 'bookmarks#favorites', as: 'favorites'
   get 'bookmarks', to: 'bookmarks#index', as: 'index'
   post 'bookmarks', to: 'bookmarks#create', as: 'bookmarks'
   delete 'bookmarks/:id', to: 'bookmarks#destroy', as: 'bookmark'
